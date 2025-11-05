9f0656713f1df6739a0599f2a4494a7f1ec88bbe
gugugamer933-ux <gugugamer933@gmail.com>
2025-11-05 14:18:21 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // força gerar site estático
-  images: {
-    unoptimized: true
-  }
-};
-
-module.exports = nextConfig;
